0d6e4eafc6474da79501afe929fd08196772a36f
Hojae Kim <hojakim@c6r6s1.42seoul.kr>
2023-01-31 07:31:49 +0900

    bckup

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hojakim <hojakim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 16:51:23 by hojakim           #+#    #+#             */
-/*   Updated: 2023/01/30 21:00:47 by hojakim          ###   ########.fr       */
+/*   Updated: 2023/01/30 21:42:52 by hojakim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,26 +19,62 @@
 
 # define BUFFER_SIZE 10
 
-int	check_line(char *buf)
+int	check_endline(char *buf, int len)
 {
-	while ()
+	int	i;
+
+	i = 0;
+	while (i < len)
+	{
+		if (buf[i] == '\n')
+			return (i);
+		i++;
+	}
 	return (-1);
+}
+
+char	*join_str(char *s1, char *s2, size_t s1_len, size_t s2_len)
+{
+	char	*str;
+	int		i;
+
+	str = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
+	i = 0;
+	while (i < s1_len)
+		str[i] = s1[i];
+	i = 0;
+	while (i < s2_len)
+		str[s1_len + i] = s2[i];
+	return (str);
 }
 
 char	*get_next_line(int fd)
 {
 	char	*buf;
+	char	*result;
 	int		len;
+	int		idx;
 
 	if (!fd)
 		return (NULL);
 	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buf)
 		return (NULL);
-	len = read(fd, buf, BUFFER_SIZE);
-	buf[BUFFER_SIZE] = '\0';
-	printf("len : %d, %s\n", len, buf);
-	return (buf);
+	while (0 < (len = read(fd, buf, BUFFER_SIZE)))
+	{
+		
+	}
+	idx = check_endline(buf, len);
+	if (idx > 0)
+	{
+		buf[idx] = '\0';
+	}
+	else
+	{
+		buf[BUFFER_SIZE] = '\0';
+	}
+	printf("bf_size : %d len : %d\n%s\n", BUFFER_SIZE, len, buf);
+	return (result);
 }
 
 int	main(int argc, char const *argv[])
